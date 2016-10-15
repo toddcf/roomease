76a884fb7ffc3a91eee70a0770c7a0d49da45e86
Michelle Luo <mluo0301@gmail.com>
2016-10-15 11:49:20 -0700

    handlebar swtich

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
+var models  = require('../models');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+
+
+
+router.get('/', function(req, res) {
+
+  // SOLUTION:
+  // =========
+  // use the burgertable model to find all burgers,
+  // and use the include option to grab info from the User model.
+  // This will let us show the cat and it's owner.
+  models.matchtable.findAll({
+  })
+  // connect the findAll to this .then
+  .then(function(matchtable) {
+    // grab the user info from our req.
+    // How is it in our req?
+    // This info gets saved to req via the users_controller.js file.
+    res.render('index', {
+      // burger_name: req.session.burger_name,
+      matchtable:matchtable
+    });
+  });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var models  = require('../models');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
 
 module.exports = router;
