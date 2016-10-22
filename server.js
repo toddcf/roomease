224c3e5faa8f1495f5e070b2a0ca0b857a8a74af
Michelle Luo <mluo0301@gmail.com>
2016-10-22 01:17:04 -0700

    michellefinlize

--- server.js
+++ server.js
@@ -1,26 +1,14 @@
-/* 6- Sequelize with Cats
- *  (((SOLUTION)))
- * -/-/-/-/-/-/-/-/-/-/-/ */
 
-
-/* STUDENTS
- * Your instructions lie in users_controller.js
- * and cats_controller.js
- * Good luck!
- * -/-/-/-/-/ */
-
-
-
-// Dependencies
-// ============
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser'); // for working with cookies
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session'); 
-var methodOverride = require('method-override'); // for deletes in express
+var methodOverride = require('method-override');
+// var bparser = require('body-parser')
+// var nodeMail = require('nodemailer')
 
 
 // Our model controllers (rather than routes)
@@ -87,6 +75,12 @@ app.use(function(err, req, res, next) {
     error: (app.get('env') === 'development') ? err : {}
   });
 });
+
+
+
+
+
+
 
 
 // our module get's exported as app.
