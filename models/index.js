7831452418b75c6a32487608f954a0e1de598f05
Todd Croak-Falen <Todd Croak-Falen>
2016-10-21 21:47:48 -0700

    Cleaned up comments and standardized formatting in all the models files.

--- models/index.js
+++ models/index.js
@@ -7,30 +7,30 @@ var env       = process.env.NODE_ENV || "development";
 var config    = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
 
 if(process.env.JAWSDB_URL)
-  var sequelize = new Sequelize(process.env.JAWSDB_URL);
+    var sequelize = new Sequelize(process.env.JAWSDB_URL);
 else
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+    var sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 
 var db        = {};
 
 fs
-  .readdirSync(__dirname)
-  .filter(function(file) {
-    return (file.indexOf(".") !== 0) && (file !== "index.js");
-  })
-  .forEach(function(file) {
-    var model = sequelize.import(path.join(__dirname, file));
-    db[model.name] = model;
-  });
+    .readdirSync(__dirname)
+    .filter(function(file) {
+        return (file.indexOf(".") !== 0) && (file !== "index.js");
+    })
+    .forEach(function(file) {
+        var model = sequelize.import(path.join(__dirname, file));
+        db[model.name] = model;
+    });
 
 Object.keys(db).forEach(function(modelName) {
-  if ("associate" in db[modelName]) {
-    db[modelName].associate(db);
-  }
+    if ("associate" in db[modelName]) {
+        db[modelName].associate(db);
+    }
 });
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+db.sequelize    = sequelize;
+db.Sequelize    = Sequelize;
 
-module.exports = db;
+module.exports  = db;
